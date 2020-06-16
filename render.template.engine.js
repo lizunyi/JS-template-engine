e1e28582f2c9d37b532e2f71a774543f99994376
lizunyi <lizunyi@qianyitravel.com>
2020-06-16 12:26:38 +0800

    支持单行，循环，嵌套循环（代码层面）

--- render.template.engine.js
+++ render.template.engine.js
@@ -1,33 +1,27 @@
 $(function(){
-	function renderValue(content,rowBind,rowFormat){
+	function renderValue(content,rowBind){
 		content = content.trim();
 		for(let item in rowBind){
 			let itemVal;
 			let itemExp = rowBind[item];
-			if(!$.isArray(itemExp)){
-				if(rowFormat && rowFormat[item]){
-					rowFormat[item].call(rowBind,item,rowBind);
-				}else{
-					if(typeof itemExp == "function"){
-						itemVal = rowBind.item.call(rowBind,item,rowBind);
-					}else{
-						itemVal = itemExp;
-					}
-				}
-				let regex = new RegExp("{"+item+"}","g");
-				content = content.replace(regex,itemVal);
+			if(typeof itemExp == "function"){
+				itemVal = itemExp.call(rowBind,item,rowBind);
+			}else{
+				itemVal = itemExp;
 			}
+			let regex = new RegExp("{"+item+"}","g");
+			content = content.replace(regex,itemVal);
+			
 		}
 		return content;
 	}
 
-	$.renderTemplateEngine = function(id,rowBind,format){
-		format = format || {};
+	$.renderTemplateEngine = function(id,rowBind){
 		let template = $("#"+id).html().trim();
 		if(!rowBind){
 			return template;
 		}
-		let regex = /{for[\s]*([\S]*)[\s]*(.*{\w+}*[^}]*)}/;
+		let regex = /{for[\s]*([\S]*)[\s]*(.*{\w+}*[\s\S]*)}/;
 		let array = [];
 		do{
 			array = regex.exec(template);
@@ -37,14 +31,13 @@ $(function(){
 				let data = rowBind[var_v];
 				let result = [];
 				for(let i in data){
-					result.push(renderValue(var_c,data[i],format[var_v]));
+					result.push(renderValue(var_c,data[i]));
 				}
 				template = template.replace(regex,result.join(""));
 			}
 		}while(array && array.length);
 		
-		template = renderValue(template,rowBind,format);
-			
+		template = renderValue(template,rowBind);
 		return template;
 	}
 });
